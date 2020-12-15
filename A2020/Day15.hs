6ba00fd6a161848318a06feb4ea6ba261d0be5e1
Ondřej Šebek <xsebek@fi.muni.cz>
2020-12-15 13:37:31 +0100

    Solve day fifteen part two

--- A2020/Day15.hs
+++ A2020/Day15.hs
@@ -1,14 +1,15 @@
 -- |
 -- Module      : Day15
--- Description : Solution to AOC 2020 Day 15: ?????
+-- Description : Solution to AOC 2020 Day 15: Rambunctious Recitation
 -- Maintainer  : <xsebek@fi.muni.cz>
 --
 -- <https://adventofcode.com/2020/day/15>
 module Day15 where
 
+import Criterion.Main (bench, bgroup, defaultMain, whnf)
 import qualified Data.IntMap.Strict as Map
 
-type Spoken = Map.IntMap [Int]
+type Spoken = Map.IntMap Int
 
 -- | Solution to Day 15.
 main15 :: FilePath -> IO ()
@@ -16,6 +17,13 @@ main15 f = do
   input <- parse <$> readFile f
   print $ solve1 input
   print $ solve2 input
+  defaultMain
+    [ bgroup
+        "15"
+        [ bench "part1" $ whnf solve1 input
+        --, bench "part2" $ whnf solve2 input -- only try on powerful computer
+        ]
+    ]
 
 -- >>> parse example
 -- [0,3,6]
@@ -27,38 +35,34 @@ type Ix = Int
 -- >>> solve1 $ parse example
 -- 436
 solve1 :: [Int] -> Ix
-solve1 = fst . head . runUntil 2020
+solve1 = runUntil 2020
 
--- >>> map fst $ runUntil 10 $ parse example
--- [0,4,0,1,3,3,0,6,3,0]
-runUntil ::
-  Int -> -- stop at index
-  [Int] -> -- input
-  [(Int, Ix)] -- (Number, Index)
-runUntil i input = newUntil i m rev
+-- >>> runUntil 10 $ parse example
+-- 0
+runUntil :: Int -> [Int] -> Int
+runUntil i input = newUntil i m (head rev)
   where
     rev = reverse $ zip input [1 ..]
-    m = Map.fromList $ tail $ map (fmap (: [])) rev
+    m = Map.fromList $ tail rev
 
-newUntil :: Int -> Spoken -> [(Int, Ix)] -> [(Int, Ix)]
-newUntil i sp r = if snd (head nr) == i then nr else newUntil i nsp nr
+newUntil :: Int -> Spoken -> (Int, Ix) -> Int
+newUntil i sp (num, ix) = if six == i then nr else newUntil i nsp (nr, six)
   where
-    (nsp, nr) = new sp r
-    new sp res@((num, ix) : _) = (: res) . (,succ ix) <$> nextNumber sp num ix
+    !six = succ ix
+    (!nsp, !nr) = nextNumber sp num ix
 
--- >>> nextNumber (Map.fromList [(0,[1]), (3,[2])]) 6 3
--- (fromList [(0,[1]),(3,[2]),(6,[3])],0)
+-- >>> nextNumber (Map.fromList [(0,1), (3,2)]) 6 3
+-- (fromList [(0,1),(3,2),(6,3)],0)
 nextNumber :: Spoken -> Int -> Ix -> (Spoken, Int)
-nextNumber spoken num ix = (newSpoken, next)
+nextNumber spoken !num !ix = (newSpoken, next)
   where
-    next = case Map.findWithDefault [] num spoken of
-      [] -> 0 --error $ unwords [show index, show num]
-      (x : _) -> ix - x -- error $ unwords [show index, show num, show x]
-    newSpoken = Map.alter (Just . maybe [ix] (ix :)) num spoken
+    !next = maybe 0 (ix -) (Map.lookup num spoken)
+    !newSpoken = Map.insert num ix spoken
 
--- >>> solve2 example
-solve2 :: a -> Int
-solve2 = undefined
+-- >>> solve2 $ parse example
+-- ProgressCancelledException
+solve2 :: [Int] -> Int
+solve2 = runUntil 30000000
 
 example :: String
 example = "0,3,6"
