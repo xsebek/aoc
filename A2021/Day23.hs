9d9e23ab8e650552f2c5683d6b6e74cc0c81bc87
Ondřej Šebek <xsebek@fi.muni.cz>
2022-11-27 15:33:10 +0100

    Day 23 part 2

--- A2021/Day23.hs
+++ A2021/Day23.hs
@@ -13,78 +13,112 @@ import Data.Bifunctor (second)
 import Data.Char (isSpace)
 import Data.Map (Map)
 import Data.Map qualified as Map
-import Data.Maybe (mapMaybe)
+import Data.Maybe (isNothing, mapMaybe, maybeToList, fromMaybe)
 import Data.Set (Set)
 import Data.Set qualified as Set
 import Linear
 import Optics
+import Text.Read (readMaybe)
 
 -- | Solution to Day 23.
 main23 :: FilePath -> IO ()
 main23 f = do
-  input <- parse <$> readFile f
-  --mapM_ (\i -> print i >> (case solveCheapest i example of Just (c,bs) -> print c >> mapM_ pretty bs; _ -> putStrLn "Nothing")) [1..16]
-  mapM_ (\i -> print i >> (case solveCheapest i input of Just (c, bs) -> print c >> mapM_ pretty bs; _ -> putStrLn "Nothing")) [15 .. 20]
-  print $ solve1 input
-  print $ solve2 input
+  inputs <- readFile f
+  -- let input1 = parse inputs
+  -- let solve input = mapM_ (\i -> print i >> (case solveCheapest i input of Just (c, bs) -> print c >> mapM_ pretty bs; _ -> putStrLn "Nothing"))
+  -- solve input1 [15,14..0]
+  -- let input2 = let l = lines inputs in parse . unlines $ take 3 l <> hiddenLines <> drop 3 l
+  -- solve (input2{organized = organized2}) [35]
+  print $ solve1 inputs
+  print $ solve2 inputs
 
 type P2 = V2 Int
 
 data Amphipod = A | B | C | D
   deriving (Eq, Ord, Enum, Show, Read)
 
-data SpaceF a
+data Space
   = Wall
-  | Hall a
-  | Space a
-  deriving (Eq, Ord, Functor, Show)
+  | Hall
+  | Space
+  deriving (Eq, Ord, Show)
 
-type Space = SpaceF (Maybe Amphipod)
+type Pods = Map P2 Amphipod
 
-type Burrow = Map P2 Space
+data Burrows = Burrows
+  { burrow :: Map P2 Space
+  , pods :: Map P2 Amphipod
+  , organized :: Map P2 Amphipod
+  , isSSpace :: P2 -> Bool
+  , isSHall :: P2 -> Bool
+  , maxX :: Int
+  , maxY :: Int
+  }
 
-parse :: String -> Burrow
-parse s = Map.fromList l
+parse :: String -> Burrows
+parse s =
+  Burrows
+    { burrow = b
+    , pods = ps
+    , organized = organized1
+    , isSSpace = (`Set.member` spaces)
+    , isSHall = (`Set.member` halls)
+    , maxX = maximumD _x
+    , maxY = maximumD _y
+    }
  where
-  l = [(V2 x y, parseSpace c) | (y, ls) <- zip [0 ..] (lines s), (x, c) <- zip [0 ..] ls, not (isSpace c)]
-  parseSpace c
-    | c == '#' = Wall
-    | c == '.' = Hall Nothing
-    | c == '_' = Space Nothing
-    | otherwise = Space (Just $ read [c])
+  ps = Map.fromList [(V2 x y, p) | (y, ls) <- zip [0 ..] (lines s), (x, c) <- zip [0 ..] ls, p <- maybeToList (readMaybe [c])]
+  b = Map.fromList [(V2 x y, parseSpace c) | (y, ls) <- zip [0 ..] (lines s), (x, c) <- zip [0 ..] ls, not (isSpace c)]
+  halls = Map.keysSet $ Map.filter (== Hall) b
+  spaces = Map.keysSet $ Map.filter (== Space) b
+  parseSpace = \case
+    '#' -> Wall
+    '.' -> Hall
+    _ -> Space
+  maximumD :: LensVL' P2 Int -> Int
+  maximumD d = maximum . map (view $ lensVL d) $ Map.keys b
 
 type Path = [P2]
 
 neighbors :: P2 -> [P2]
 neighbors (V2 x y) = [V2 (x + i) (y + j) | i <- [-1, 0, 1], j <- [-1, 0, 1], abs (i + j) == 1]
 
-empty :: Space -> Bool
-empty = \case
-  Hall Nothing -> True
-  Space Nothing -> True
-  _ -> False
-
-paths :: Burrow -> Set P2 -> P2 -> [Path]
-paths b s p = map (p :) ps
+paths :: Burrows -> Set P2 -> P2 -> [Path]
+paths b@Burrows{..} s p = map (p :) ps
  where
   s' = Set.insert p s
-  ns = filter (`Set.notMember` s) $ neighbors p
-  pos = map fst . filter (empty . snd) $ zip ns (map (b Map.!) ns)
+  pos = filter (\p2 -> empty p2 && Set.notMember p2 s && Map.notMember p2 pods) $ neighbors p
   ps = map (: []) pos <> concatMap (paths b s') pos
+  empty p2 = isSSpace p2 || isSHall p2
 
-amphiPaths :: Burrow -> [(Amphipod, Path)]
-amphiPaths b = splits $ hpaths' <> bpaths
+amphiPaths :: Burrows -> [(Amphipod, Path)]
+amphiPaths b@Burrows{..} = splits $ hpaths' <> bpaths
  where
-  pods f = mapMaybe f $ Map.toList b
-  hpods = pods (\case (p, Hall (Just a)) -> Just (a, p); _ -> Nothing)
-  bpods = pods (\case (p, Space (Just a)) -> Just (a, p); _ -> Nothing)
+  lpods f = mapMaybe f $ Map.toList pods
+  hpods = lpods (\(p, a) -> if isSHall p then Just (a, p) else Nothing) -- (\case (p, Hall (Just a)) -> Just (a, p); _ -> Nothing)
+  bpods = lpods (\(p, a) -> if isSSpace p then Just (a, p) else Nothing) -- (\case (p, Space (Just a)) -> Just (a, p); _ -> Nothing)
   filterL c = filter (c . last)
   podPaths cond = map $ second (filterL (not . aboveBurrow) . filterL cond . paths b mempty)
-  aboveBurrow p = isSHall' p && isSSpace' (p + V2 0 1)
-  hpaths = podPaths ((== Space Nothing) . (b Map.!)) hpods
-  hpaths' = map (\(a, ps) -> (a,) $ filter (\p -> not (pathFromDone b (a, p)) && organized Map.! last p == Space (Just a)) ps) hpaths
-  bpaths = podPaths ((== Hall Nothing) . (b Map.!)) bpods
+  aboveBurrow p = isSHall p && isSSpace (p + V2 0 1)
+  bpaths = podPaths isSHall bpods
+  hpaths = podPaths isSSpace hpods
+  hpaths' = map (\(a, ps) -> (a,) $ filter (\p -> let ap = (a, p) in notPathFromDone b ap && pathToHome b ap) ps) hpaths
   splits = concatMap (\(a, bs) -> map (a,) bs)
+
+notPathFromDone :: Burrows -> (Amphipod, Path) -> Bool
+notPathFromDone Burrows{..} (_a, ps) =
+  let (V2 x _) = last ps
+      slope = [V2 x y | y <- [2 .. maxY - 1]]
+      same p = pods Map.!? p  == organized Map.!? p
+   in not $ all same slope
+
+pathToHome :: Burrows -> (Amphipod, Path) -> Bool
+pathToHome b ap = go (move ap b) (last $ snd ap)
+ where
+  go Burrows{..} (V2 x _) =
+    let slope = [V2 x y | y <- [2 .. maxY - 1]]
+        same p = let sp = pods Map.!? p in isNothing sp || sp == organized Map.!? p
+     in all same slope
 
 amphiCost :: (Amphipod, Path) -> Int
 amphiCost (a, p) = cost a * (length p - 1)
@@ -96,116 +130,74 @@ cost = \case
   C -> 100
   D -> 1000
 
-move :: (Amphipod, Path) -> Burrow -> Burrow
+move :: (Amphipod, Path) -> Burrows -> Burrows
 move (a, p) b =
   b
-    & Map.update (swaps $ Just a) (last p)
-    & Map.update (swaps Nothing) (head p)
- where
-  swaps :: Maybe Amphipod -> Space -> Maybe Space
-  swaps m = Just . fmap (const m)
+    { pods =
+        pods b
+          & Map.insert (last p) a
+          & Map.delete (head p)
+    }
 
-solveCheapest :: Int -> Burrow -> Maybe (Int, [Burrow])
-solveCheapest steps =
-  fmap (second $ map snd)
-    . dijkstraAssoc
+solveCheapest :: Int -> Burrows -> Maybe (Int, [Burrows])
+solveCheapest steps burs =
+  second (map (setp . snd))
+    <$> dijkstraAssoc
       assocStep
-      -- (getMinimum . snd)
-      ((== organized) . snd)
-    . (0,)
+      ((== organized burs) . snd)
+      (1, pods burs)
  where
-  assocStep :: (Int, Burrow) -> [((Int, Burrow), Int)]
-  assocStep (n, b) = if n > steps then [] else map (\ap -> ((n + 1, move ap b), amphiCost ap)) $ amphiPaths b
+  setp p = burs{pods = p}
+  assocStep :: (Int, Pods) -> [((Int, Pods), Int)]
+  assocStep (n, p) = if n > steps then [] else map (\ap -> ((n + 1, pods $ move ap (setp p)), amphiCost ap)) $ amphiPaths (setp p)
 
-pathFromDone :: Burrow -> (Amphipod, Path) -> Bool
-pathFromDone b (_a, ps) = startsInGood
- where
-  start = head ps
-  lower = start + V2 0 1
-  same p = b Map.! p == organized Map.! p
-  startsInGood = same start && same lower
-
-halls :: Set P2
-halls = Map.keysSet $ Map.filter isSHall emptyBurrow
-
-spaces :: Set P2
-spaces = Map.keysSet $ Map.filter isSSpace emptyBurrow
-
-isSHall :: Space -> Bool
-isSHall = \case Hall _ -> True; _ -> False
-
-isSSpace :: Space -> Bool
-isSSpace = \case Space _ -> True; _ -> False
-
-isSHall' :: P2 -> Bool
-isSHall' p = Set.member p halls
-
-isSSpace' :: P2 -> Bool
-isSSpace' p = Set.member p spaces
-
-home :: Amphipod -> [P2]
-home a = Map.keys $ Map.filter (== Space (Just a)) organized
-
-homes :: [[P2]]
-homes = map home [A .. D]
-
-pathHome :: (P2, Amphipod) -> Int
-pathHome (p, a) = (cost a *) . pred . minimum $ map (sum . fmap abs . subtract p) (homes !! fromEnum a)
-
-getMinimum :: Burrow -> Int
-getMinimum b = sum $ map pathHome aps
- where
-  s2ma :: Space -> Maybe Amphipod
-  s2ma = \case
-    Wall -> Nothing
-    Hall ma -> ma
-    Space ma -> ma
-  aps = Map.toList $ Map.mapMaybe s2ma b
+solveCheapest' :: Burrows -> (Int, [Burrows])
+solveCheapest' b = fromMaybe (error "Impossible to solve!") $ solveCheapest (2 * length (pods b)) b
 
 -- >>> solve1 example
-solve1 :: a -> Int
-solve1 = errorWithoutStackTrace "Part 1 not implemented"
+solve1 :: String -> Int
+solve1 = fst . solveCheapest' . parse
+
+hiddenLines :: [String]
+hiddenLines =
+  [ "  #D#C#B#A#"
+  , "  #D#B#A#C#"
+  ]
 
 -- >>> solve2 example
-solve2 :: a -> Int
-solve2 = errorWithoutStackTrace "Part 2 not implemented"
+solve2 :: String -> Int
+solve2 input = fst $ solveCheapest' (hidden{organized = organized2})
+  where
+    l = lines input
+    hidden = parse . unlines $ take 3 l <> hiddenLines <> drop 3 l
 
 prettySpace :: Space -> Char
 prettySpace = \case
   Wall -> '#'
-  Hall m_am -> maybe '.' (head . show) m_am
-  Space m_am -> maybe '_' (head . show) m_am
+  Hall -> '.'
+  Space -> '_'
 
-pretty :: Burrow -> IO ()
-pretty b =
+pretty :: Burrows -> IO ()
+pretty Burrows{..} =
   mapM_
     putStrLn
-    [ [ maybe ' ' prettySpace ms
+    [ [ if Map.member p pods then head . show $ pods Map.! p else maybe ' ' prettySpace ms
       | x <- [0 .. maximumD _x]
-      , let ms = Map.lookup (V2 x y) b
+      , let p = V2 x y
+      , let ms = Map.lookup p burrow
       ]
     | y <- [0 .. maximumD _y]
     ]
  where
   maximumD :: LensVL' P2 Int -> Int
-  maximumD d = maximum . map (view $ lensVL d) $ Map.keys b
+  maximumD d = maximum . map (view $ lensVL d) $ Map.keys burrow
 
-prettyPaths :: Burrow -> IO ()
+prettyPaths :: Burrows -> IO ()
 prettyPaths b = mapM_ (\ap@(a, p) -> print a >> print p >> print (amphiCost ap) >> pretty (move ap b)) $ amphiPaths b
 
-emptyBurrow :: Burrow
-emptyBurrow =
-  parse . unlines $
-    [ "#############"
-    , "#...........#"
-    , "###_#_#_#_###"
-    , "  #_#_#_#_#"
-    , "  #########"
-    ]
-
-organized :: Burrow
-organized =
-  parse . unlines $
+organized1 :: Pods
+organized1 =
+  pods . parse . unlines $
     [ "#############"
     , "#...........#"
     , "###A#B#C#D###"
@@ -213,7 +205,19 @@ organized =
     , "  #########"
     ]
 
-example :: Burrow
+organized2 :: Pods
+organized2 =
+  pods . parse . unlines $
+    [ "#############"
+    , "#...........#"
+    , "###A#B#C#D###"
+    , "  #A#B#C#D#"
+    , "  #A#B#C#D#"
+    , "  #A#B#C#D#"
+    , "  #########"
+    ]
+
+example :: Burrows
 example =
   parse . unlines $
     [ "#############"
