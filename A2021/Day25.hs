ee63e554c66a1b6bab07b4b369d79bd5e2767ab2
Ondřej Šebek <xsebek@fi.muni.cz>
2022-11-28 11:18:24 +0100

    Solve day 25

--- A2021/Day25.hs
+++ A2021/Day25.hs
@@ -6,6 +6,9 @@
 -- <https://adventofcode.com/2021/day/25>
 module Day25 where
 
+import Data.Array (assocs, bounds, (!), (//))
+import Data.Array.Unboxed (Array, array)
+
 -- | Solution to Day 25.
 main25 :: FilePath -> IO ()
 main25 f = do
@@ -13,16 +16,107 @@ main25 f = do
   print $ solve1 input
   print $ solve2 input
 
-parse :: String -> [Int]
-parse = map read . lines
+parse :: String -> SeaFloor
+parse = seaFloor . concatMap (zipWith (\x (y, c) -> ((x, y), parseCell c)) [0 ..]) . zipWith (map . (,)) [0 ..] . lines
+
+seaFloor :: [(P, Cell)] -> SeaFloor
+seaFloor nncs = array (zero, maxP) nncs
+ where
+  maxP = fst $ last nncs
+
+type P = (Int, Int)
+
+zero :: P
+zero = (0, 0)
+
+data Cell = Empty | CucumberSouth | CucumberEast
+  deriving (Eq, Ord, Show, Enum)
+
+parseCell :: Char -> Cell
+parseCell = \case
+  '>' -> CucumberEast
+  'v' -> CucumberSouth
+  '.' -> Empty
+  c -> error $ "Unknown character: " <> show c
+
+cellChar :: Cell -> Char
+cellChar = \case
+  Empty -> '.'
+  CucumberSouth -> 'v'
+  CucumberEast -> '>'
+
+type SeaFloor = Array P Cell
+
+prettyFloor :: SeaFloor -> String
+prettyFloor s =
+  let ((x1, y1), (x2, y2)) = bounds s
+   in unlines
+        [ [ cellChar c
+          | i <- [x1 .. x2]
+          , let c = s ! (i, j)
+          ]
+        | j <- [y1 .. y2]
+        ]
+
+-- ----------------------------------------------------------------------------
+-- PART 1
+-- ----------------------------------------------------------------------------
 
 -- >>> solve1 example
-solve1 :: a -> Int
-solve1 = errorWithoutStackTrace "Part 1 not implemented"
+-- 58
+solve1 :: SeaFloor -> Int
+solve1 = length . moveUntilStop
+
+moveEast :: SeaFloor -> SeaFloor
+moveEast s = s // updates
+ where
+  (_min, (mx, _my)) = bounds s
+  updates = concatMap move allEast
+  allEast = map fst . filter ((== CucumberEast) . snd) $ assocs s
+  move p@(x, y) =
+    let pe = ((x + 1) `mod` (mx + 1), y)
+     in if s ! pe == Empty then [(p, Empty), (pe, CucumberEast)] else []
+
+moveSouth :: SeaFloor -> SeaFloor
+moveSouth s = s // updates
+ where
+  (_min, (_mx, my)) = bounds s
+  updates = concatMap move allSouth
+  allSouth = map fst . filter ((== CucumberSouth) . snd) $ assocs s
+  move p@(x, y) =
+    let ps = (x, (y + 1) `mod` (my + 1))
+     in if s ! ps == Empty then [(p, Empty), (ps, CucumberSouth)] else []
+
+moveAll :: SeaFloor -> SeaFloor
+moveAll = moveSouth . moveEast
+
+moveUntilStop :: SeaFloor -> [SeaFloor]
+moveUntilStop s =
+  let ss = iterate moveAll s
+      (d,e) = span (uncurry (/=)) $ zip ss (tail ss)
+  in map fst $ d <> take 1 e
+
+-- ----------------------------------------------------------------------------
+-- PART 2
+-- ----------------------------------------------------------------------------
 
 -- >>> solve2 example
 solve2 :: a -> Int
 solve2 = errorWithoutStackTrace "Part 2 not implemented"
 
-example :: a
-example = undefined
+example :: SeaFloor
+example =
+  parse . unlines $
+    [ "v...>>.vv>"
+    , ".vv>>.vv.."
+    , ">>.>v>...v"
+    , ">>v>>.>.v."
+    , "v>v.vv.v.."
+    , ">.>>..v..."
+    , ".vv..>.>v."
+    , "v.v..>>v.v"
+    , "....v..v.>"
+    ]
+
+exampleLine :: SeaFloor
+exampleLine = parse "...>>>>>..."
