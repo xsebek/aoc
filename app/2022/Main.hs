fc2ac26f3b4ef81bf009b5d59c3e0ea2c6c16e89
Ondřej Šebek <xsebek@fi.muni.cz>
2022-12-02 23:38:13 +0100

    Prepare day 03

--- app/2022/Main.hs
+++ app/2022/Main.hs
@@ -9,7 +9,7 @@ import System.Environment (getArgs)
 
 import Day01 (main01)
 import Day02 (main02)
--- import Day03 (main03)
+import Day03 (main03)
 -- import Day04 (main04)
 -- import Day05 (main05)
 -- import Day06 (main06)
@@ -53,7 +53,7 @@ solutionsM =
   apply
     [ main01
     , main02
-    , todoDay -- main03
+    , main03
     , todoDay -- main04
     , todoDay -- main05
     , todoDay -- main06
