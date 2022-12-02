e326e1ea62945467899c5f20adfd3b87cff1d459
Ondřej Šebek <xsebek@fi.muni.cz>
2022-12-02 23:11:32 +0100

    Solve day 02 part 2

--- A2022/Day02.hs
+++ A2022/Day02.hs
@@ -15,9 +15,10 @@ main02 f = do
 
 parse :: String -> [(L, R)]
 parse = map (two . words) . lines
-  where
-    two [l, r] = (read l, read r) 
-    two ls = error $ "Expected two words, got: " <> show ls
+ where
+  two [l, r] = (read l, read r)
+  two ls = error $ "Expected two words, got: " <> show ls
+
 data L = A | B | C
   deriving (Eq, Ord, Enum, Bounded, Show, Read)
 
@@ -27,12 +28,12 @@ data R = X | Y | Z
 -- >>> solve1 example
 -- 15
 solve1 :: [(L, R)] -> Int
-solve1 = sum . map score
+solve1 = sum . map score1
 
--- >>> map score example
+-- >>> map score1 example
 -- [8,1,6]
-score :: (L, R) -> Int
-score lr = scoreShape + scorePlay
+score1 :: (L, R) -> Int
+score1 lr = scoreShape + scorePlay
  where
   scoreShape = 1 + fromEnum (snd lr)
   scorePlay = case play lr of
@@ -43,15 +44,33 @@ score lr = scoreShape + scorePlay
 -- >>> map play example
 -- [GT,LT,EQ]
 play :: (L, R) -> Ordering
-play (l, r) = compare 1 $ (fromEnum l - fromEnum r + 1) `mod` 3 
+play (l, r) = compare 1 $ (fromEnum l - fromEnum r + 1) `mod` 3
 
 -- >>> solve2 example
-solve2 :: a -> Int
-solve2 = errorWithoutStackTrace "Part 2 not implemented"
+-- 12
+solve2 :: [(L, R)] -> Int
+solve2 = sum . map score2
+
+-- >>> map score2 example
+-- [4,1,7]
+score2 :: (L, R) -> Int
+score2 lr = scoreShape + scorePlay
+ where
+  scoreShape = 1 + fromEnum (replay lr)
+  scorePlay = case snd lr of
+    X -> 0
+    Y -> 3
+    Z -> 6
+
+-- >>> map replay example
+-- [A,A,A]
+replay :: (L, R) -> L
+replay (l, r) = toEnum $ (fromEnum l + fromEnum r - 1) `mod` 3
 
 example :: [(L, R)]
-example = parse . unlines $
-  [ "A Y"
-  , "B X"
-  , "C Z"
-  ]
+example =
+  parse . unlines $
+    [ "A Y"
+    , "B X"
+    , "C Z"
+    ]
