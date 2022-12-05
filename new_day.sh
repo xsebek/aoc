41ae2aea85c7ddad868b85e770e42787b7d3b70a
Ondřej Šebek <xsebek@fi.muni.cz>
2022-12-05 20:46:18 +0100

    Automate new day preparation

---
+++ new_day.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set +x
+
+YEAR=$(date '+%Y')
+DAY=$(date '+%d')
+
+cp "A${YEAR}/DayDAY.hs" "A${YEAR}/Day${DAY}.hs"
+
+sed -i "s/DAY/${DAY}/g" "A${YEAR}/Day${DAY}.hs"
+
+sed -i \
+  "s/-- import Day${DAY}/import Day${DAY}/g; s/todoDay -- main${DAY}/main${DAY}/g" \
+  "app/${YEAR}/Main.hs"
+
+sed -i "s/-- Day${DAY}/Day${DAY}/g" 'aoc.cabal'
