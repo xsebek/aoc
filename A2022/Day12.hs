c0b303a84231db40b9e366f2349c9328597b0b49
Ondrej Sebek <ondras98@icloud.com>
2022-12-12 21:56:16 +0100

    Prepare day 12

---
+++ A2022/Day12.hs
@@ -0,0 +1,28 @@
+-- |
+-- Module      : Day12
+-- Description : Solution to AOC 2022 Day 12: Hill Climbing Algorithm
+-- Maintainer  : <xsebek@fi.muni.cz>
+--
+-- <https://adventofcode.com/2022/day/12>
+module Day12 where
+
+-- | Solution to Day 12.
+main12 :: FilePath -> IO ()
+main12 f = do
+  input <- parse <$> readFile f
+  print $ solve1 input
+  print $ solve2 input
+
+parse :: String -> [Int]
+parse = map read . lines
+
+-- >>> solve1 example
+solve1 :: a -> Int
+solve1 = errorWithoutStackTrace "Part 1 not implemented"
+
+-- >>> solve2 example
+solve2 :: a -> Int
+solve2 = errorWithoutStackTrace "Part 2 not implemented"
+
+example :: a
+example = undefined
